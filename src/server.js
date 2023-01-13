cb444eadfb6c84146ae1d5029683741afec583e3
Mikael Josias <mikaeljosias2002@gmail.com>
2023-01-12 21:17:08 -0300

    added new users into the database collection using a post method

--- src/server.js
+++ src/server.js
@@ -1,20 +1,28 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import Joi from "joi";
+import dotenv from "dotenv";
+dotenv.config();
+
+//==================== INTERFACES ====================\\
+
+const schema = Joi.object({
+    name: Joi.string().min(3).max(15).required(),
+})
 
 //==================== DATABASE ====================\\
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
-const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
-mongoClient.connect()
-    .then(() => {
-        db = mongoClient.db("herois");
-        console.log("Conexão realizada com Sucesso!");
-    })
-    .catch(() => {
-        console.log("Não foi possivel se conectar ao Banco de Dados!");
-    });
+try {
+    await mongoClient.connect();
+    db = mongoClient.db();
+    console.log("Conectado ao Banco de Dados!");
+} catch (err) {
+    console.log(err);    
+}
 
 //==================== SERVER ====================\\
 
@@ -22,6 +30,33 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.post("/participants", async (req, res) => {
+    const {name} = req.body;
+
+    //verifica se o nome é vazio
+    if (schema.validate({name}).error) {
+        return res.status(422).send("Usuário não pode ser vázio!");
+    }
+
+    try {
+        //verifica se usuário já está cadastrado
+        if (await db.collection("participants").findOne({name})) {
+            return res.status(409).send("Usuário já está cadastrado!");
+        }
+
+        //cadastra o novo usuário
+        const newUser = {
+            name,
+            lastStatus: Date.now(),
+        }
+
+        await db.collection("participants").insertOne(newUser);
+    } catch (err) {
+        console.log(err);
+    }
+
+    res.send("OK");
+})
 
 server.listen(5000, () => {
     console.log("Server Inicializado 🚀!!!");
